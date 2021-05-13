5b105e7643a50a3c27abd486346d631fa04ff06c
nojiri <yuta0458@gmail.com>
2021-05-13 15:25:29 +0900

    events_controller

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,8 +1,17 @@
 class Api::EventsController < ApplicationController
   def index
     events = Event.all
+
+    render json: {
+      events: events
+    }, status: :ok
   end
 
   def show
+    event = Event.find(params[:event_id])
+
+    render json: {
+      event: event
+    }, status: :ok
   end
 end
