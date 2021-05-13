2742457fd8ba570479009c081d185406615ff7f6
nojiri <yuta0458@gmail.com>
2021-05-13 16:59:15 +0900

    orders_controller

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,8 +1,14 @@
 class Api::OrdersController < ApplicationController
+    # 注文番号を取得して支払い済みにする
     def is_purchased
+        order = Order.find(params[:order_id])
+        order.update(isPurchased: true)
     end
 
+    # カートの購入ボタンが押された時に注文を作る
     def create
+        order = Order.new(cart_id: @cart.id, isPurchased: false)
+        order.save
     end
 
 end
