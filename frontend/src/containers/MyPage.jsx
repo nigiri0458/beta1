2e8e2b4ff7384c5e62c504a1b08afb24e7dcb8ba
nojiri <yuta0458@gmail.com>
2021-06-13 00:16:39 +0900

    mypage (not completed)

--- frontend/src/containers/MyPage.jsx
+++ frontend/src/containers/MyPage.jsx
@@ -1,5 +1,6 @@
-import {React, useEffect, useReducer} from 'react';
+import {React, useEffect, useReducer, useState} from 'react';
 import { Link } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
 
 import { initialState, usersActionTypes, usersReducer } from '../reducers/users';
 import { REQUEST_STATE } from '../constants';
@@ -25,18 +26,79 @@ export const Mypage = () => {
         );
     }, [])
 
+    const [usernameEditorOpen, setUsernameEditorOpen] = useState(false);
+    const [usernameModalOpen, setUsernameModalOpen] = useState(false);
+    const [emailEditorOpen, setEmailEditorOpen] = useState(false);
+    const [emailModalOpen, setEmailModalOpen] = useState(false);
+    const [username, setUsername] = useState(state.userInfo.username);
+    const [email, setEmail] = useState(state.userInfo.email);
+
+    const usernameModalSubmit = () => {
+
+    }
+
+    const usernameModalClose = () => {
+        setUsernameModalOpen(false);
+        setUsernameEditorOpen(false);
+    }
+
     return(
         <div className="mypage-wrapper">
             <p className="mypage-title">マイページ MyPage</p>
             <h2 className="mypage-username">ユーザー名 Username</h2>
+            {
+                usernameModalOpen ?
+                <div className="mypage-modal">
+                    <p className="mypage-modal-message">ユーザー名を{username}に変更します。よろしいですか？</p>
+                    <button className="mypage-submit-button" onClick={() => usernameModalSubmit()}>はい<br/>Yes</button>
+                    <button className="mypage-submit-button" onClick={() => usernameModalClose()}>いいえ<br/>No</button>
+                </div>
+                :
+                <div>
+                </div>
+            }
             <div className="mypage-edit-form">
-                <p className="mypage-username2">{state.userInfo.username}</p>
-                <button className="mypage-edit-button">変更<br/>Edit</button>
+                {
+                    usernameEditorOpen ?
+                    <div>
+                        <TextField 
+                            id="standard-basic"
+                            label=""
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <button className="mypage-edit-button1" onClick={() => setUsernameModalOpen(true)}>確定<br/>Submit</button>
+                        <button className="mypage-edit-button2" onClick={() => setUsernameEditorOpen(false)}>キャンセル<br/>Cancel</button>
+                    </div>
+                    :
+                    <div>
+                        <p className="mypage-username2">{state.userInfo.username}</p>
+                        <button className="mypage-edit-button3" onClick={() => setUsernameEditorOpen(true)}>変更<br/>Edit</button>
+                    </div>
+                }
             </div>
             <h2 className="mypage-email">メールアドレス Email</h2>
             <div className="mypage-edit-form">
-                <p className="mypage-email2">{state.userInfo.email}</p>
-                <button className="mypage-edit-button">変更<br/>Edit</button>
+                {
+                    emailEditorOpen ?
+                    <div>
+                        <TextField 
+                            id="standard-basic"
+                            label=""
+                            type="text"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <button className="mypage-edit-button1" onClick={() => setEmailEditorOpen(false)}>確定<br/>Submit</button>
+                        <button className="mypage-edit-button2" onClick={() => setEmailEditorOpen(false)}>キャンセル<br/>Cancel</button>
+                    </div>
+                    :
+                    <div>
+                        <p className="mypage-email2">{state.userInfo.email}</p>
+                        <button className="mypage-edit-button3" onClick={() => setEmailEditorOpen(true)}>変更<br/>Edit</button>
+                    </div>
+                }
             </div>
             
             <Link to="/cart" className="mypage-cart-link">
