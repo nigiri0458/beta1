9359009d6bbea2b09b787ba6e1a9bd1057dcde79
nojiri <yuta0458@gmail.com>
2021-06-18 11:43:12 +0900

    cloudinary settings

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+    config.cloud_name = "handshake"
+    config.api_key = "331334532595775"
+    config.api_secret = "ZD3yb8P8-zQB8cMS5QyQY1Aob4A"
+    config.secure = true
+    config.cdn_subdomain = true
+end
