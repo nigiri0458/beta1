e2e4ad1605f86e1253c8d02e0c37b626d806abc7
hirotoiwashi <hiroto1028@outlook.jp>
2021-08-06 01:09:35 +0900

    test git

--- frontend/src/components/Top.jsx
+++ frontend/src/components/Top.jsx
@@ -2,7 +2,7 @@ import {React, useEffect, useState} from 'react';
 import { useSelector } from 'react-redux';
 import {Link} from 'react-router-dom';
 import '../styles/Top.css';
-
+// gitのテスト
 export const Top = () => {
     useEffect(() => {
         console.log(localStorage.getItem("loginState"));
